1b577f3d335b4af8a0f6183a2a6c2e6bc8cee5b6
NastyaKatyushkina <nastyusha_kat@mail.ru>
2023-06-15 15:44:18 +0500

    time

--- src/components/WithTimePretty.js
+++ src/components/WithTimePretty.js
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs';
-const relativeTime = require('dayjs/plugin/relativeTime');
-dayjs.extend(relativeTime);
 
 function WithTimePretty(Component) {
+    const relativeTime = require('dayjs/plugin/relativeTime');
+    dayjs.extend(relativeTime);
     return function (props) {
         const date = dayjs(props.date).fromNow();
         return <Component {...props} date={date} />;
